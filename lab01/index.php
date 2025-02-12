fe1d48a9dea692d437d4d359687d2c5fcaae74fb
mihalovp04 <mihailovp04@gmail.com>
2025-02-12 19:13:24 +0200

    lab1

---
+++ lab01/index.php
@@ -0,0 +1,13 @@
+<?php
+echo "Привет, мир!<br>";
+echo "Hello, World with echo!<br>";
+print "Hello, World with print!<br>";
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+echo "Ещё " . $days . " дней. " . $message . "<br>";
+
+echo "Ещё $days дней. $message<br>";
+
+?>
